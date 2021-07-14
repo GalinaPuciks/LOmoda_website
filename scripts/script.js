3863d9f204e8ca862e8c52478ad4df18a984dfa8
Galina <shaman282861@gmail.com>
2021-07-15 00:34:00 +0200

    scroll

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,30 @@ headerCityButton.addEventListener('click', () => {
     localStorage.setItem('lomoda-location', city)
 });
 
+// скролл
+
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+    document.body.dbScrollY = window.scrollY;
+    
+    document.body.style.cssText = `
+        position: fixed;
+        top: ${-window.scrollY}px;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        padding-right: ${widthScroll}px;
+    `;
+};
+
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY,
+    })
+};
+
 // модальное окно
 
 const subheaderCart = document.querySelector('.subheader__cart');
