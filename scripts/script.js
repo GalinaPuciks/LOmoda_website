04cd077a5a67a54feacc672c737ac9ba9dac0ba2
Galina <shaman282861@gmail.com>
2021-07-15 00:03:23 +0200

    modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
 const headerCityButton = document.querySelector('.header__city-button');
 
-headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?'
+headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 
 headerCityButton.addEventListener('click', () => {
     const city = prompt('Укажите ваш город');
     headerCityButton.textContent = city;
     localStorage.setItem('lomoda-location', city)
 });
+
+// модальное окно
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+};
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+
+    if(target.matches('.cart__btn-close') || target.matches('.cart-overlay')){
+        cartModalClose();
+    }
+}); 
