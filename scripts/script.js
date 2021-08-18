dce7cf845ae08bb4f7f13dfe479e2ff795e7ef6f
Galina <shaman282861@gmail.com>
2021-08-18 23:25:31 +0200

    hash done

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 const headerCityButton = document.querySelector('.header__city-button');
 
+let hash = location.hash.substring(1);
+
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 
 headerCityButton.addEventListener('click', () => {
@@ -50,7 +52,7 @@ const cartModalClose = () => {
 //запрос базы данных
 
 const getData = async () => {
-    const data = await fetch("db.json");
+    const data = await fetch("../db.json");
 
     if(data.ok) {
         return data.json();
@@ -59,25 +61,21 @@ const getData = async () => {
     }
 };
 
-const getGoods = (callback) => {
+const getGoods = (callback, value) => {
     getData()
     .then(data => {
-        callback(data);
+        if(value) {
+            callback(data.filter(item => item.category === value))
+        } else {
+            callback(data);
+        }
+        
     })
     .catch(err => {
         console.error(err)
     });
 };
 
-subheaderCart.addEventListener('click', cartModalOpen);
-
-cartOverlay.addEventListener('click', event => {
-    const target = event.target;
-
-    if(target.matches('.cart__btn-close') || target.matches('.cart-overlay')){
-        cartModalClose();
-    }
-}); 
 
 
 try {
@@ -88,20 +86,28 @@ try {
         throw "This is not a goods page!"
     }
 
-    const createCard = data => {
-        console.log(data);
+    const createCard = ({id, preview, cost, brand, name, sizes}) => {
+   
+      
+
+      
+
         const li = document.createElement("li");
         li.classList.add("goods__item");
         li.innerHTML = `
         <article class="good">
-                            <a class="good__link-img" href="card-good.html#id56454">
-                                <img class="good__img" src="goods-image/AD002EMLUEA8_14164246_1_v1.jpg" alt="">
+                            <a class="good__link-img" href="card-good.html#${id}">
+                                <img class="good__img" src="goods-image/${preview}" alt="">
                             </a>
                             <div class="good__description">
-                                <p class="good__price">2890 &#8381;</p>
-                                <h3 class="good__title">Eazyway <span class="good__title__grey">/ Тайтсы</span></h3>
-                                <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">40 42 44 46</span></p>
-                                <a class="good__link" href="card-good.html#id56454">Подробнее</a>
+                                <p class="good__price">${cost} &#8381;</p>
+                                <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                                ${sizes ?
+                                    ` <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(" ")}</span></p>`:
+                                    ""
+                                }
+                               
+                                <a class="good__link" href="card-good.html#${id}">Подробнее</a>
                             </div>
                         </article>
         `;
@@ -111,15 +117,20 @@ try {
     };
     
     const renderGoodsList = data => {
-        goodsList.textContent = "";
+        goodsList.textContent = '';
 
-     data.forEach(item => {
-        const card = createCard(item);
-        goodsList.append(card);
+        data.forEach(item => {
+            const card = createCard(item);
+            goodsList.append(card);
      });
     };
 
-    getGoods(renderGoodsList);
+
+window.addEventListener("hashchange", () =>{
+    hash = location.hash.substring(1);
+    getGoods(renderGoodsList, hash);
+})
+    getGoods(renderGoodsList, hash);
 } catch(err) {
 
 }
